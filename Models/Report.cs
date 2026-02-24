bc8917a15e11fa16ac4168b70cbd2ca758d2982a
Jannie Brunhøj <72923791+ArtTondar@users.noreply.github.com>
2026-02-24 15:13:49 +0100

    Add project files.

---
+++ Models/Report.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Report
+    {
+        public int BRNumber { get; set; }
+        public string? PrimaryUrl { get; set; }
+        public string? FallbackUrl { get; set; }
+        public int Year { get; set; }
+        public StatusMessage Status { get; set; } = StatusMessage.NotDownloaded;
+        public string LocalPath { get; set; }
+        public bool IsLocalFile { get; set; }
+
+        //LastAttempt og RetryCount - resume/retry
+        public DateTime? LastAttempt { get; set; }
+        public int RetryCount { get; set; }
+
+        //Logging og statistik
+        public double? FileSizeKB { get; set; }
+        public string Domain { get; set; }
+        public double? DownloadTimeSeconds { get; set; }
+
+    }
+    public enum StatusMessage
+    {
+        NotDownloaded,
+        Downloaded,
+        Failed
+    }
+}
